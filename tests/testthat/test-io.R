b8b34c9318c142632ef4212f20775134627196ad
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-22 13:13:41 +0100

    More IO test cases

--- tests/testthat/test-io.R
+++ tests/testthat/test-io.R
@@ -38,3 +38,66 @@ test_that("We can get the error stream", {
   out <- sort(p$read_error_lines())
   expect_identical(out, c("hello", "world"))
 })
+
+test_that("Output & error at the same time", {
+
+  skip_on_cran()
+  skip_other_platforms("unix")
+
+  tmp <- tempfile(fileext = ".sh")
+  on.exit(unlink(tmp), add = TRUE)
+
+  cat(
+    ">&2 echo hello",
+    "echo wow",
+    ">&2 echo world",
+    "echo wooow",
+    sep = "\n", file = tmp
+  )
+  Sys.chmod(tmp, "700")
+
+  p <- process$new(tmp, stdout = TRUE, stderr = TRUE)
+  on.exit(try_silently(p$kill(grace = 0)), add = TRUE)
+
+  p$wait()
+  out <- p$read_output_lines()
+  expect_identical(out, c("wow", "wooow"))
+
+  err <- p$read_error_lines()
+  expect_identical(err, c("hello", "world"))
+})
+
+test_that("Output and error to specific files", {
+
+  skip_on_cran()
+  skip_other_platforms("unix")
+
+  tmp <- tempfile(fileext = ".sh")
+  on.exit(unlink(tmp), add = TRUE)
+
+  cat(
+    ">&2 echo hello",
+    "echo wow",
+    ">&2 echo world",
+    "echo wooow",
+    sep = "\n", file = tmp
+  )
+  Sys.chmod(tmp, "700")
+
+  tmpout <- tempfile()
+  tmperr <- tempfile()
+
+  p <- process$new(tmp, stdout = tmpout, stderr = tmperr)
+  on.exit(try_silently(p$kill(grace = 0)), add = TRUE)
+
+  p$wait()
+
+  out <- p$read_output_lines()
+  expect_identical(out, c("wow", "wooow"))
+
+  err <- p$read_error_lines()
+  expect_identical(err, c("hello", "world"))
+
+  expect_identical(readLines(tmpout), c("wow", "wooow"))
+  expect_identical(readLines(tmperr), c("hello", "world"))
+})
