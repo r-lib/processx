9f9ea712ad7ea59a015878a98bc70c147291221a
Gábor Csárdi <csardi.gabor@gmail.com>
2016-09-07 14:23:46 +0100

    Take the first pid if multiple processes

--- R/get_pid.R
+++ R/get_pid.R
@@ -89,7 +89,7 @@ get_pid_by_name_windows <- function(name, children) {
   if (length(pids) >= 1) pids else NULL
 }
 
-#' @importFrom utils tail
+#' @importFrom utils head
 
 get_pid_by_name_unix <- function(name, children) {
 
@@ -129,7 +129,7 @@ get_pid_by_name_unix <- function(name, children) {
       paste(pid, collapse = ", "),
       ", this should not happen"
 )
-    tail(pid, 1)
+    head(pid, 1)
   }
 }
 
