d15d9808d2c4fd9578b5bb0ff2abd47dcd0f91b5
Gábor Csárdi <csardi.gabor@gmail.com>
2016-11-18 11:45:47 +0000

    Refactor startup, document it, wait with timeout
    * Refactor the initialize method to a new file.
    * Document the startup tricks we use.
    * wait_for_file(): wait for a file, with a timeout.
      This is used to wait for the pid files.

---
+++ R/initialize.R
@@ -0,0 +1,252 @@
+
+#' Start a process
+#'
+#' @param self this
+#' @param private this$private
+#' @param command Command to run, string scalar.
+#' @param args Command arguments, character vector.
+#' @param commandline Alternative to command + args.
+#' @param stdout Standard output, FALSE to ignore, TRUE for temp file.
+#' @param stderr Standard error, FALSE to ignore, TRUE for temp file.
+#' @param cleanup Kill on GC?
+#'
+#' @keywords internal
+#' @importFrom utils head tail
+
+process_initialize <- function(self, private, command, args,
+                               commandline, stdout, stderr, cleanup) {
+
+  "!DEBUG process_initialize `command`"
+
+  assert_string_or_null(command)
+  assert_character(args)
+  assert_flag_or_string(stdout)
+  assert_flag_or_string(stderr)
+  assert_string_or_null(commandline)
+  assert_flag(cleanup)
+
+  if (is.null(command) + is.null(commandline) != 1) {
+    stop("Need exactly one of 'command' and 'commandline")
+  }
+  if (!is.null(commandline) && ! identical(args, character())) {
+    stop("Omit 'args' when 'commandline' is specified")
+  }
+
+  private$command <- command
+  private$args <- args
+  private$commandline <- commandline
+  private$cleanup <- cleanup
+  private$pstdout <- stdout
+  private$pstderr <- stderr
+
+  if (isTRUE(stdout)) {
+    private$cleanfiles <- c(private$cleanfiles, stdout <- tempfile())
+  }
+  if (isTRUE(stderr)) {
+    private$cleanfiles <- c(private$cleanfiles, stderr <- tempfile())
+  }
+
+  cmd <- if (!is.null(command)) {
+    paste0(
+      shQuote(command), " ",
+      if (length(args)) paste(shQuote(args), collapse = " ")
+    )
+
+  } else {
+    paste0("(", commandline, ")")
+  }
+
+  pidfile <- tempfile()
+  fullcmd <- paste0(
+    get_my_pid_code(pidfile),
+    cmd, " ",
+    " >",  if (isFALSE(stdout)) null_file() else shQuote(stdout),
+    " 2>", if (isFALSE(stderr)) null_file() else shQuote(stderr),
+    "\n"
+  )
+
+  ## Create temporary file to run
+  ## Do NOT remove this with on.exit(), because that creates a race
+  ## condition that hits back at you on Windows: the shell might not
+  ## start running before the file is deleted by on.exit.
+  cmdfile <- tempfile(fileext = ".bat")
+
+  ## Add command to it, make it executable
+  cat(fullcmd, file = cmdfile)
+  Sys.chmod(cmdfile, "700")
+
+  "!DEBUG process_initialize system()"
+  ret <- system(
+    paste(shQuote(cmdfile), shQuote(basename(cmdfile)), "2>&1"),
+    wait = FALSE
+  )
+  if (ret != 0) stop("Cannot start process")
+
+  ## Get the pid from the pid file, this also removes it
+  pids <- get_pid_from_file(pidfile, cmdfile)
+  private$pid <- head(pids, 1)
+  "!DEBUG process_initialize get_pid_from_file(): `private$pid`"
+
+  ## Cleanup on GC, if requested
+  if (cleanup) {
+    reg.finalizer(
+      self,
+      function(e) {
+        "!DEBUG killing"
+        e$kill()
+      },
+      TRUE
+    )
+  }
+
+  ## Store the output and error files, we'll open them later if needed
+  private$stdout <- stdout
+  private$stderr <- stderr
+
+  invisible(self)
+}
+
+## ---------------------------------------------------------------------
+
+#' Platform-dependent code to output the PID to a file
+#'
+#' [get_pid_form_file()] will read the PID from the file.
+#' You need this function, because the format of the file is
+#' platform dependent.
+#'
+#' @param pidfile The file that will contain the pid
+#'   `paste0(pidfile, "3")` will be used to notify that `pidfile`
+#'   is complete and contains the PID. (Otherwise it might be an
+#'   empty file.)
+#' @return Numeric PID
+#'
+#' @family pid queries
+#' @keywords internal
+
+get_my_pid_code <- function(pidfile) {
+  if (os_type() == "unix") {
+    get_my_pid_code_unix(pidfile)
+  } else {
+    get_my_pid_code_windows(pidfile)
+  }
+}
+
+#' Some tricks in the windows version:
+#' * we output all child processes of the R session into the output
+#'   file, and will use a random id, that is an argument to the R script,
+#'   to extract the correct child.
+#' * `wmic` outputs UTF-16 text to the file, so we run `type` in it, and
+#'   redirect it to another file, which will be ASCII already.
+#'
+#' @rdname get_my_pid_code
+
+get_my_pid_code_windows <- function(pidfile) {
+  script <- paste0(
+    "wmic /output:%s process where '(parentprocessid=%d)'",
+    "  get commandline, processid\n",
+    "type %s > %s\n",
+    "echo done > %s\n"
+  )
+  pidfile2 <- paste0(pidfile, "2")
+  pidfile3 <- paste0(pidfile, "3")
+  sprintf(script, shQuote(pidfile2), Sys.getpid(),
+          shQuote(pidfile2), shQuote(pidfile),
+          shQuote(pidfile3))
+}
+
+#' The unix version is straightforward, as a unix shell knows its
+#' process id.
+#'
+#' @rdname get_my_pid_code
+
+get_my_pid_code_unix <- function(pidfile) {
+  script <- paste0(
+    "echo $$ > %s\n",
+    "echo done > %s\n"
+  )
+  sprintf(script, shQuote(pidfile), shQuote(paste0(pidfile, "3")))
+}
+
+## ---------------------------------------------------------------------
+
+#' Parse a PID from an output file written by [get_my_pid()]
+#'
+#' The format of the file is platform dependent.
+#'
+#' @param pidfile Name of the pid file, same as the argument to
+#'   [get_my_pid_file()].
+#' @param cmdfile Name of the script file, we use this as an ID on
+#'   windows, to find the process among the children of the R process,
+#'   because windows shells cannot report their own process id.
+#'
+#' @family pid queries
+#' @keywords internal
+
+get_pid_from_file <- function(pidfile, cmdfile) {
+  "!DEBUG get_pid_from_file"
+  if (os_type() == "unix") {
+    get_pid_from_file_unix(pidfile, cmdfile)
+  } else {
+    get_pid_from_file_windows(pidfile, cmdfile)
+  }
+}
+
+#' @rdname get_pid_from_file
+#' @importFrom utils tail
+
+get_pid_from_file_windows <- function(pidfile, cmdfile) {
+
+  pidfile2 <- paste0(pidfile, "2")
+  pidfile3 <- paste0(pidfile, "3")
+
+  ## Wait for the 'done' file to be there, with a timeout
+  wait_for_file(pidfile3)
+
+  pidhandle <- file(pidfile2, open = "r")
+  on.exit(
+    {
+      close(pidhandle)
+      try(unlink(pidfile), silent = TRUE)
+      try(unlink(pidfile2), silent = TRUE)
+      try(unlink(pidfile3), silent = TRUE)
+    },
+    add = TRUE
+  )
+
+  token <- basename(cmdfile)
+  while (length(l <- readLines(pidhandle, n = 1)) &&
+           !grepl(token, l, fixed = TRUE)) {
+    NULL
+  }
+
+  id <- if (length(l)) {
+    as.numeric(tail(strsplit(str_trim(l), " ")[[1]], 1))
+  }
+  if (is.null(id) || is.na(id)) {
+    stop("Cannot find pid, internal processx error")
+  }
+  c(id, get_children(id))
+}
+
+#' @rdname get_pid_from_file
+
+get_pid_from_file_unix <- function(pidfile, cmdfile) {
+
+  pidfile3 <- paste0(pidfile, "3")
+  on.exit(
+    {
+      try(unlink(pidfile), silent = TRUE)
+      try(unlink(pidfile3), silent = TRUE)
+    },
+    add = TRUE
+  )
+
+  ## Wait for the 'done' file to be there
+  wait_for_file(pidfile3)
+
+  pids <- as.numeric(readLines(pidfile, n = 1))
+
+  ## This should not happen, but just to be sure that we do not
+  ## kill the R process itself
+  setdiff(pids, Sys.getpid())
+}
--- R/utils.R
+++ R/utils.R
@@ -35,3 +35,12 @@ check_tool <- function(x) {
 str_trim <- function(x) {
   sub("\\s+$", "", sub("^\\s+", "", x))
 }
+
+wait_for_file <- function(file, check_interval = 0.01, timeout = 10) {
+  tries <- max(timeout / check_interval, 1)
+  for (i in 1:tries) {
+    if (file.exists(file)) return(invisible(TRUE))
+    Sys.sleep(check_interval)
+  }
+  stop("File was not created in ", timeout, " secs: ", file)
+}
