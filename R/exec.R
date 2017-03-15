c4f469ce8bda9844fbe949568ea2366348e09306
Gabor Csardi <csardi.gabor@gmail.com>
2017-03-15 13:03:05 +0000

    Win: hide windows by default
    Because in Rgui or RStudio we do not want a cmd to open...

--- R/exec.R
+++ R/exec.R
@@ -1,7 +1,7 @@
 
 exec <- function(command, args = character(), stdout = NULL, stderr = NULL,
                  detached = FALSE, windows_verbatim_args = FALSE,
-		 windows_hide_window = FALSE) {
+		 windows_hide_window = TRUE) {
 
   .Call("processx_exec", command, c(command, args), stdout, stderr,
         detached, windows_verbatim_args, windows_hide_window, PACKAGE = "processx")
