1825b1051e13a1decff5e098b101ae9e04ad7cc8
Winston Chang <winston@stdout.org>
2017-03-23 20:22:16 -0500

    Add class to external pointer

--- R/named_pipe.R
+++ R/named_pipe.R
@@ -3,17 +3,30 @@
 
 #' @useDynLib processx C_createNamedPipe
 create_named_pipe <- function(name, mode = "r") {
-  .Call(C_createNamedPipe, name, mode)
+  structure(
+    .Call(C_createNamedPipe, name, mode),
+    class = "named_pipe"
+  )
 }
 
 
 #' @useDynLib processx C_closeNamedPipe
-close_named_pipe <- function(text, pipe = NULL) {
-  .Call(C_closeNamedPipe, text, pipe)
+close_named_pipe <- function(pipe = NULL) {
+  if (!is.named_pipe(pipe))
+    stop("`pipe` must be a named_pipe object.")
+
+  .Call(C_closeNamedPipe, pipe)
 }
 
 
 #' @useDynLib processx C_writeNamedPipe
 write_named_pipe <- function(text, pipe = NULL) {
+  if (!is.named_pipe(pipe))
+    stop("`pipe` must be a named_pipe object.")
+
   .Call(C_writeNamedPipe, text, pipe)
+}
+
+is.named_pipe <- function(x) {
+  inherits(x, "named_pipe")
 }
