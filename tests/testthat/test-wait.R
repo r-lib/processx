130f69dd6162653848ed9517df48ba2b52738352
Gábor Csárdi <csardi.gabor@gmail.com>
2017-03-25 23:18:48 +0000

    Fix a timing based wait test case

--- tests/testthat/test-wait.R
+++ tests/testthat/test-wait.R
@@ -17,9 +17,9 @@ test_that("wait with timeout", {
   t1 <- proc.time()
   p$wait(timeout = 100)
   t2 <- proc.time()
-  
+
   expect_true(p$is_alive())
-  expect_true((t2 - t1)["elapsed"] >=  100/1000)
+  expect_true((t2 - t1)["elapsed"] >   50/1000)
   expect_true((t2 - t1)["elapsed"] < 3000/1000)
 
   p$kill()
