ea66cf14ba105691b2cf45462443d1951269d481
Winston Chang <winston@stdout.org>
2017-04-14 12:39:33 -0500

    Split up named_pipe.c in win and unix

--- src/win/named_pipe.c
+++ src/win/named_pipe.c
@@ -1,3 +1,4 @@
+
 #ifdef _WIN32
 
 #include <stdlib.h>
@@ -136,32 +137,6 @@ SEXP processx_write_named_pipe(SEXP pipe_ext, SEXP text) {
     FlushFileBuffers(hPipe);
 
     return text;
-}
-
-
-#else
-// On non-windows platforms, we still need the C interfaces, but they simply
-// give errors.
-#include <Rdefines.h>
-
-SEXP processx_is_named_pipe_open(SEXP pipe_ext) {
-    error("processx_is_named_pipe_open only valid on Windows.");
-    return R_NilValue;
-}
-
-SEXP processx_close_named_pipe(SEXP pipe_ext) {
-    error("processx_close_named_pipe only valid on Windows.");
-    return R_NilValue;
-}
-
-SEXP processx_create_named_pipe(SEXP name, SEXP mode) {
-    error("processx_create_named_pipe only valid on Windows.");
-    return R_NilValue;
-}
-
-SEXP processx_write_named_pipe(SEXP pipe_ext, SEXP text) {
-    error("processx_write_named_pipe only valid on Windows.");
-    return R_NilValue;
 }
 
 #endif
