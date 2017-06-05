475015b304b977aa9dde6e8130c4ef0246c644fb
Gábor Csárdi <csardi.gabor@gmail.com>
2017-06-05 09:19:47 -0700

    Run tests on CRAN windows
    To see if they work....

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(processx)
 
-## Do not run tests on CRAN Windows
-
-if (Sys.getenv("NOT_CRAN", "") != "" || .Platform$OS.type != "windows") {
-  test_check("processx")
-}
+test_check("processx")
