17214e4b6e00d079e8737e72297150439193e114
Winston Chang <winston@stdout.org>
2017-07-31 22:36:52 -0500

    Add support for Windows network paths

--- R/utils.R
+++ R/utils.R
@@ -26,25 +26,44 @@ last_char <- function(x) {
   substring(x, nc, nc)
 }
 
-# Given a filename, return full path to that file. On Windows, this includes the
-# drive. It currently doesn't handle Windows network paths (start with "//").
-# Also, only "/" is allowed (no backslashes in the path).
+# Given a filename, return an absolute path to that file. This has two important
+# differences from normalizePath(). (1) The file does not need to exist, and (2)
+# the path is merely absolute, whereas normalizePath() returns a canonical path,
+# which resolves symbolic links, gives canonical case, and, on Windows, may give
+# short names.
+#
+# On Windows, the returned path includes the drive ("C:") or network server
+# ("//myserver"). Only "/" is supported as a path separator (no backslashes).
 full_path <- function(path) {
+  assert_that(is_string(path))
+
   # Try expanding "~"
   path <- path.expand(path)
 
   # If relative path, prepend current dir. On Windows, also record current
   # drive.
   if (is_windows()) {
-    if (grepl("^[a-zA-Z]:/", path)) {
+    if (grepl("^[a-zA-Z]:", path)) {
       drive <- substring(path, 1, 2)
       path <- substring(path, 3)
+
+    } else if (substring(path, 1, 2) == "//") {
+      # Extract server name, like "//server", and use as drive.
+      pos <- regexec("^(//[^/]*)(.*)", path)[[1]]
+      drive <- substring(path, pos[2], attr(pos, "match.length", exact = TRUE)[2])
+      path <- substring(path, pos[3])
+
+      # Must have a name, like "//server"
+      if (drive == "//")
+        stop("Server name not found in network path.")
+
     } else {
       drive <- substring(getwd(), 1, 2)
 
       if (substr(path, 1, 1) != "/")
         path <- substring(file.path(getwd(), path), 3)
     }
+
   } else {
     if (substr(path, 1, 1) != "/")
       path <- file.path(getwd(), path)
@@ -52,10 +71,10 @@ full_path <- function(path) {
 
   parts <- strsplit(path, "/")[[1]]
 
-  # Collapse any ".." and "." in path.
+  # Collapse any "..", ".", and "" in path.
   i <- 2
   while (i <= length(parts)) {
-    if (parts[i] == ".") {
+    if (parts[i] == "." || parts[i] == "") {
       parts <- parts[-i]
 
     } else if (parts[i] == "..") {
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -8,24 +8,53 @@ test_that("full_path gives correct values", {
     # Use "" so that file.path("", "a") will return "/a"
     drive <- ""
   }
+
   expect_identical(full_path("/a/b"), file.path(drive, "a/b"))
   expect_identical(full_path("/a/b/"), file.path(drive, "a/b"))
-  expect_identical(full_path("/"), paste0(drive, "/"))
+  expect_identical(full_path("/"), file.path(drive, ""))
   expect_identical(full_path("a"), file.path(getwd(), "a"))
   expect_identical(full_path("a/b"), file.path(getwd(), "a/b"))
+
   expect_identical(full_path("a/../b/c"), file.path(getwd(), "b/c"))
   expect_identical(full_path("../../../../../../../../../../../a"), file.path(drive, "a"))
   expect_identical(full_path("/../.././a"), file.path(drive, "a"))
   expect_identical(full_path("/a/./b/../c"), file.path(drive, "a/c"))
+
   expect_identical(full_path("~nonexistent_user"), file.path(getwd(), "~nonexistent_user"))
   expect_identical(full_path("~/a/../b"), path.expand("~/b"))
+
+  expect_identical(full_path("a//b"), file.path(getwd(), "a/b"))
+  expect_identical(full_path("/a//b"), file.path(drive, "a/b"))
 })
 
 test_that("full_path gives correct values, windows", {
   skip_other_platforms("windows")
 
+  # Drives
   expect_identical(full_path("f:/a/b"), "f:/a/b")
   expect_identical(full_path("f:/a/b/../../.."), "f:/")
   expect_identical(full_path("f:/../a"), "f:/a")
   expect_identical(full_path("f:/"), "f:/")
+  expect_identical(full_path("f:"), "f:/")
+
+  # Leading double slashes. Server name always has trailing slash ("//server/"),
+  # like drives do ("f:/"). But dirs on the server don't have a trailing slash.
+  expect_identical(full_path("//a"), "//a/")
+  expect_identical(full_path("//a/"), "//a/")
+  expect_identical(full_path("//a/b"), "//a/b")
+  expect_identical(full_path("//a/b/.."), "//a/")
+  # Can't go .. to remove the server name
+  expect_identical(full_path("//a/b/../.."), "//a/")
+  expect_identical(full_path("//a/../b"), "//a/b")
+  expect_error(full_path("//"))
+  expect_error(full_path("///"))
+  expect_error(full_path("///a"))
+})
+
+test_that("full_path gives correct values, unix", {
+  skip_other_platforms("unix")
+
+  # Leading double slashes should collapse
+  expect_identical(full_path("//"), "/")
+  expect_identical(full_path("///a/"), "/a")
 })
