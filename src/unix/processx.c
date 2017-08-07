e301000806f434ce5cdae3472bba80e8ff08eef5
Gábor Csárdi <csardi.gabor@gmail.com>
2017-08-07 16:37:21 +0100

    Unix: fix a potential invalid read
    When the process object is freed before
    the connection objects, and we poll.

--- src/unix/processx.c
+++ src/unix/processx.c
@@ -204,6 +204,8 @@ static SEXP processx__make_handle(SEXP private, int cleanup) {
 
 static void processx__handle_destroy(processx_handle_t *handle) {
   if (!handle) return;
+  if (handle->std_out) handle->std_out->process = 0;
+  if (handle->std_err) handle->std_err->process = 0;
   free(handle);
 }
 
