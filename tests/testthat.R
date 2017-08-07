5141cce7158120e238d3beb16b26c3e1bb21baba
Gábor Csárdi <csardi.gabor@gmail.com>
2017-08-07 14:53:22 +0100

    Run tests on Windows, always

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,5 @@
 library(testthat)
 library(processx)
 
-if (Sys.getenv("NOT_CRAN") != "" || .Platform$OS.type != "windows") {
-  test_check("processx")
-}
+Sys.setenv("R_TESTS" = "")
+test_check("processx")
