82b1389ce03d1ec3b15ea637f020f54406f935ab
Gábor Csárdi <csardi.gabor@gmail.com>
2017-10-18 12:27:28 +0100

    Remove unused code

--- src/poll.c
+++ src/poll.c
@@ -1,41 +1,6 @@
 
 #include "processx.h"
 
-/* Create a pollable from a process object
- *
- * @param pollable Pointer to the object to fill.
- * @param process The input process object.
- * @param which Which stream to poll. Possible values:
- *   * 0: stdin (not implemented yet)
- *   * 1: stdout
- *   * 2: stderr
- *   * 3: control connection (not implemented yet)
- */
-
-int processx_c_pollable_from_process(
-  processx_pollable_t *pollable,
-  processx_handle_t *process,
-  int which) {
-
-  switch (which) {
-  case 0:
-    error("Polling `stdin` is not implemented yet");
-    break;
-  case 1:
-    return processx_c_pollable_from_connection(pollable, process->pipes[1]);
-  case 2:
-    return processx_c_pollable_from_connection(pollable, process->pipes[2]);
-  case 3:
-    error("Polling the control connection is not implemented yet");
-    break;
-  default:
-    error("Invalid connection requested for polling");
-    break;
-  }
-
-  return 0;
-}
-
 SEXP processx_poll(SEXP statuses, SEXP ms) {
   int cms = INTEGER(ms)[0];
   int i, num_proc = LENGTH(statuses);
