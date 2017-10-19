742e69a8f5b13beba3a45107f78ccd07608104f8
Gabor Csardi <csardi.gabor@gmail.com>
2017-10-19 11:50:23 +0100

    Run connection tests on windows as well

--- tests/testthat/test-connections.R
+++ tests/testthat/test-connections.R
@@ -2,41 +2,41 @@
 context("Connections")
 
 test_that("lot of text", {
-  if (os_type() != "unix") skip("Only Unix")
+  skip_if_no_cat()
   txt <- strrep("x", 100000)
   cat(txt, file = tmp <- tempfile())
 
-  p <- process$new("cat", tmp, stdout = "|")
+  p <- process$new(cat_command(), tmp, stdout = "|")
   out <- p$read_all_output_lines()
 
   expect_equal(txt, out)
 })
 
 test_that("UTF-8", {
-  if (!l10n_info()$`UTF-8`) skip("Only on UTF-8 platforms")
-  if (os_type() != "unix") skip("Only Unix")
+  skip_if_no_cat()
 
-  txt <- strrep(paste0("\u00a0\u2192", "\xf0\x90\x84\x82"), 20000)
-  cat(txt, file = tmp <- tempfile())
+  txt <- charToRaw(strrep("\xc2\xa0\xe2\x86\x92\xf0\x90\x84\x82", 20000))
+  writeBin(txt, con = tmp <- tempfile())
 
-  p <- process$new("cat", tmp, stdout = "|")
+  p <- process$new(cat_command(), tmp, stdout = "|", encoding = "UTF-8")
   out <- p$read_all_output_lines()
 
-  expect_equal(txt, out)
+  expect_equal(txt, charToRaw(out))
 })
 
 test_that("UTF-8 multibyte character cut in half", {
-  if (!l10n_info()$`UTF-8`) skip("Only on UTF-8 platforms")
-  if (os_type() != "unix") skip("Only Unix")
+  skip_if_no_cat()
 
-  rtxt <- charToRaw("a\u00a0a")
+  rtxt <- charToRaw("a\xc2\xa0a")
 
   writeBin(rtxt[1:2], tmp1 <- tempfile())
   writeBin(rtxt[3:4], tmp2 <- tempfile())
 
-  p <- process$new("cat", c(tmp1, tmp2), stdout = "|")
+  p <- process$new(cat_command(), c(tmp1, tmp2), stdout = "|", encoding = "UTF-8")
   out <- p$read_all_output_lines()
   expect_equal(rtxt, charToRaw(out))
+
+  skip_other_platforms("unix")
 
   cmd <- paste("(cat", shQuote(tmp1), ";sleep 1;cat", shQuote(tmp2), ")")
   p <- process$new(commandline = cmd, stdout = "|", stderr = "|")
@@ -45,13 +45,12 @@ test_that("UTF-8 multibyte character cut in half", {
 })
 
 test_that("UTF-8 multibyte character cut in half at the end of the file", {
-  if (!l10n_info()$`UTF-8`) skip("Only on UTF-8 platforms")
-  if (os_type() != "unix") skip("Only Unix")
+  skip_if_no_cat()
 
-  rtxt <- charToRaw("a\u00a0a")
+  rtxt <- charToRaw("a\xc2\xa0a")
   writeBin(c(rtxt, rtxt[1:2]), tmp1 <- tempfile())
 
-  p <- process$new("cat", tmp1, stdout = "|")
+  p <- process$new(cat_command(), tmp1, stdout = "|", encoding = "UTF-8")
   expect_warning(
     out <- p$read_all_output_lines(),
     "Invalid multi-byte character at end of stream ignored"
@@ -60,27 +59,26 @@ test_that("UTF-8 multibyte character cut in half at the end of the file", {
 })
 
 test_that("Invalid UTF-8 characters in the middle of the string", {
-  if (!l10n_info()$`UTF-8`) skip("Only on UTF-8 platforms")
-  if (os_type() != "unix") skip("Only Unix")
+  skip_if_no_cat()
 
-  half <- charToRaw("\u00a0")[1]
+  half <- charToRaw("\xc2\xa0")[1]
   rtxt <- sample(rep(c(half, charToRaw("a")), 100))
   writeBin(rtxt, tmp1 <- tempfile())
 
-  p <- process$new("cat", tmp1, stdout = "|")
+  p <- process$new(cat_command(), tmp1, stdout = "|", encoding = "UTF-8")
   suppressWarnings(out <- p$read_all_output_lines())
 
   expect_equal(out, strrep("a", 100))
 })
 
 test_that("Convert from another encoding to UTF-8", {
-  if (os_type() != "unix") skip("Only Unix")
+  skip_if_no_cat()
 
   latin1 <- "\xe1\xe9\xed";
   writeBin(charToRaw(latin1), tmp1 <- tempfile())
 
-  p <- process$new("cat", tmp1, stdout = "|", encoding = "latin1")
+  p <- process$new(cat_command(), tmp1, stdout = "|", encoding = "latin1")
   suppressWarnings(out <- p$read_all_output_lines())
 
-  expect_equal(out, "\xc3\xa1\xc3\xa9\xc3\xad")
+  expect_equal(charToRaw(out), charToRaw("\xc3\xa1\xc3\xa9\xc3\xad"))
 })
