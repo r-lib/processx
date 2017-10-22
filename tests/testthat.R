9528de033ac5b03b66e631afa2ff79e456b0960f
Gábor Csárdi <csardi.gabor@gmail.com>
2017-10-22 23:12:22 +0100

    Wait after R CMD check tests, on windows
    This might fix an issue on win-builder.
    After the wait, all the grandchild processes
    should be done.
    
    A proper solution would recursively kill them.

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,3 +3,8 @@ library(processx)
 
 Sys.setenv("R_TESTS" = "")
 test_check("processx", reporter = "summary")
+
+## Wait until the child processes have surely finished,
+## on windows. This might fix some win-builder troubles.
+
+if (.Platform$OS.type == "windows") Sys.sleep(5)
