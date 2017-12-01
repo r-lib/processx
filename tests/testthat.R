ee4ec79334425aa901eb77777929b33ee737e778
Gábor Csárdi <csardi.gabor@gmail.com>
2017-12-01 11:43:02 +0000

    Run tests on CRAN windows

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,9 +2,7 @@ library(testthat)
 library(processx)
 
 Sys.setenv("R_TESTS" = "")
-if (Sys.getenv("NOT_CRAN") != "" || .Platform$OS.type != "windows") {
-  test_check("processx", reporter = "summary", filter = "poll2")
-}
+test_check("processx", reporter = "summary", filter = "poll2")
 
 ## Wait until the child processes have surely finished,
 ## on windows. This might fix some win-builder troubles.
