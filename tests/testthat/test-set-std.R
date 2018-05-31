52073d76975e95540d42c11c5944e60d02bf2fd7
Gábor Csárdi <csardi.gabor@gmail.com>
2018-05-31 09:34:40 +0100

    Tests for setting stdout / stderr

---
+++ tests/testthat/test-set-std.R
@@ -0,0 +1,89 @@
+
+context("set std streams")
+
+test_that("setting stdout to a file", {
+  stdout_to_file <- function(filename) {
+    con <- processx::conn_create_file(filename, write = TRUE)
+    processx::conn_set_stdout(con)
+    cat("output\n")
+    message("error")
+    close(con)
+    42
+  }
+
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  opt <- callr::r_process_options(
+    func = stdout_to_file,
+    args = list(filename = tmp))
+  on.exit(p$kill(), add = TRUE)
+  p <- callr::r_process$new(opt)
+
+  p$wait(5000)
+  expect_false(p$kill())
+  expect_equal(p$get_result(), 42)
+  expect_equal(p$read_all_error_lines(), "error")
+  expect_equal(p$read_all_output_lines(), character())
+  expect_equal(readLines(tmp), "output")
+})
+
+test_that("setting stderr to a file", {
+  stderr_to_file <- function(filename) {
+    con <- processx::conn_create_file(filename, write = TRUE)
+    processx::conn_set_stderr(con)
+    cat("output\n")
+    message("error")
+    close(con)
+    42
+  }
+
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  opt <- callr::r_process_options(
+    func = stderr_to_file,
+    args = list(filename = tmp))
+  on.exit(p$kill(), add = TRUE)
+  p <- callr::r_process$new(opt)
+
+  p$wait(5000)
+  expect_false(p$kill())
+  expect_equal(p$get_result(), 42)
+  expect_equal(p$read_all_output_lines(), "output")
+  expect_equal(p$read_all_error_lines(), character())
+  expect_equal(readLines(tmp), "error")
+})
+
+test_that("setting stdout multiple times", {
+  stdout_to_file <- function(file1, file2) {
+    con1 <- processx::conn_create_file(file1, write = TRUE)
+    processx::conn_set_stdout(con1)
+    cat("output\n")
+    message("error")
+    close(con1)
+
+    con2 <- processx::conn_create_file(file2, write = TRUE)
+    processx::conn_set_stdout(con2)
+    cat("output2\n")
+    message("error2")
+    close(con2)
+
+    42
+  }
+
+  tmp1 <- tempfile()
+  tmp2 <- tempfile()
+  on.exit(unlink(c(tmp1, tmp2)), add = TRUE)
+  opt <- callr::r_process_options(
+    func = stdout_to_file,
+    args = list(file1 = tmp1, file2 = tmp2))
+  on.exit(p$kill(), add = TRUE)
+  p <- callr::r_process$new(opt)
+
+  p$wait(5000)
+  expect_false(p$kill())
+  expect_equal(p$get_result(), 42)
+  expect_equal(p$read_all_error_lines(), c("error", "error2"))
+  expect_equal(p$read_all_output_lines(), character())
+  expect_equal(readLines(tmp1), "output")
+  expect_equal(readLines(tmp2), "output2")
+})
