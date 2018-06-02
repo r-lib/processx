83c638264edfd1bd13aa893d8fc6eaa37c6330c8
Gabor Csardi <csardi.gabor@gmail.com>
2018-06-02 11:26:02 -0700

    Fix install of interrupt.exe on windows

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,6 +1,6 @@
 
 progs <- if (WINDOWS) {
-  c(file.path("tools", "px.exe"),
+  c(file.path("tools", c("px.exe", "interrupt.exe")),
     file.path("supervisor", "supervisor.exe"))
 } else {
   c(file.path("tools", "px"),
