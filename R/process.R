ccf3eaca8b9d093003b51d34679846dd1be7c353
Gábor Csárdi <csardi.gabor@gmail.com>
2018-07-23 14:51:18 +0100

    Document kill_tree() return value

--- R/process.R
+++ R/process.R
@@ -165,6 +165,9 @@ NULL
 #' which is inherited in all child processes. This allows finding
 #' descendents, even if they are orphaned, i.e. they are not connected
 #' to the root of the tree cleanup in the process tree any more.
+#' `$kill_tree()` returns a named integer vector of the process ids that
+#' were killed, the names are the names of the processes (e.g. `"sleep"`,
+#' `"notepad.exe"`, `"Rterm.exe"`, etc.)
 #'
 #' `$wait()` waits until the process finishes, or a timeout happens.
 #' Note that if the process never finishes, and the timeout is infinite
