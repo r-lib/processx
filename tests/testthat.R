ed0ef3c1c52aae9b4d449b6d3cf27cd15427ba82
Gábor Csárdi <csardi.gabor@gmail.com>
2018-08-10 00:40:29 +0200

    Use summary reporter in check, not progress
    The output is simpler.

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,7 +4,7 @@ library(processx)
 Sys.setenv("R_TESTS" = "")
 
 if (ps::ps_is_supported()) {
-  reporter <- ps::CleanupReporter(testthat::ProgressReporter)$new()
+  reporter <- ps::CleanupReporter(testthat::SummaryReporter)$new()
 } else {
   ## ps does not support this platform
   reporter <- "summary"
