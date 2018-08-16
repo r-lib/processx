f72a7253abaf3d84c6e4161d35acbef1e9373ed9
Gábor Csárdi <csardi.gabor@gmail.com>
2018-08-16 10:14:53 +0200

    Work around a ps bug

--- R/on-load.R
+++ R/on-load.R
@@ -2,6 +2,8 @@
 Internal <- NULL
 
 .onLoad <- function(libname, pkgname) {
+  ## This is to circumvent a ps bug
+  ps::ps_handle()
   supervisor_reset()
   Internal <<- get(".Internal", asNamespace("base"))
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme() # nocov
