5917b062f4033c3c6cd71fb6b76d012611e04614
Gábor Csárdi <csardi.gabor@gmail.com>
2018-09-07 10:19:55 +0100

    Fix manual page for connections
    Roxygen silently dropped some parts...

--- R/connections.R
+++ R/connections.R
@@ -26,6 +26,7 @@ conn_create_fd <- function(fd, encoding = "", close = TRUE) {
   .Call(c_processx_connection_create_fd, fd, encoding, close)
 }
 
+#' @details
 #' `conn_create_pipepair()` creates a pair of connected connections, the
 #' first one is writeable, the second one is readable.
 #'
@@ -37,6 +38,7 @@ conn_create_pipepair <- function(encoding = "") {
   .Call(c_processx_connection_create_pipepair, encoding)
 }
 
+#' @details
 #' `conn_read_chars()` reads UTF-8 characters from the connections. If the
 #' connection itself is not UTF-8 encoded, it re-encodes it.
 #'
@@ -58,6 +60,7 @@ conn_read_chars.processx_connection <- function(con, n = -1) {
   .Call(c_processx_connection_read_chars, con, n)
 }
 
+#' @details
 #' `conn_read_lines()` reads lines from a connection.
 #'
 #' @rdname processx_connections
@@ -74,6 +77,7 @@ conn_read_lines.processx_connection <- function(con, n = -1) {
   .Call(c_processx_connection_read_lines, con, n)
 }
 
+#' @details
 #' `conn_is_incomplete()` returns `FALSE` if the connection surely has no
 #' more data.
 #'
@@ -91,6 +95,7 @@ conn_is_incomplete.processx_connection <- function(con) {
   ! .Call(c_processx_connection_is_eof, con)
 }
 
+#' @details
 #' `conn_write()` writes a character or raw vector to the connection.
 #' It might not be able to write all bytes into the connection, in which
 #' case it returns the leftover bytes in a raw vector. Call `conn_write()`
@@ -124,6 +129,7 @@ conn_write.processx_connection <- function(con, str, sep = "\n",
   invisible(.Call(c_processx_connection_write_bytes, con, str))
 }
 
+#' @details
 #' `conn_create_file()` creates a connection to a file.
 #'
 #' @param filename File name.
@@ -147,6 +153,7 @@ conn_create_file <- function(filename, read = NULL, write = NULL) {
   .Call(c_processx_connection_create_file, filename, read, write)
 }
 
+#' @details
 #' `conn_set_stdout()` set the standard output of the R process, to the
 #' specified connection.
 #'
@@ -165,6 +172,7 @@ conn_set_stdout <- function(con, drop = TRUE) {
   invisible(.Call(c_processx_connection_set_stdout, con, drop))
 }
 
+#' @details
 #' `conn_set_stderr()` set the standard error of the R process, to the
 #' specified connection.
 #'
@@ -180,6 +188,7 @@ conn_set_stderr <- function(con, drop = TRUE) {
   invisible(.Call(c_processx_connection_set_stderr, con, drop))
 }
 
+#' @details
 #' `conn_get_fileno()` return the integer file desciptor that belongs to
 #' the connection.
 #'
@@ -190,10 +199,10 @@ conn_get_fileno <- function(con) {
   .Call(c_processx_connection_get_fileno, con)
 }
 
+#' @details
 #' `conn_disable_inheritance()` can be called to disable the inheritance
 #' of all open handles. Call this function as soon as possible in a new
 #' process to avoid inheriting the inherited handles even further.
-#'
 #' The function is best effort to close the handles, it might still leave
 #' some handles open. It should work for `stdin`, `stdout` and `stderr`,
 #' at least.
