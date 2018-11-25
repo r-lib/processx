1877b84043a04c254f3e9b19d3c0f866128fadb6
Gábor Csárdi <csardi.gabor@gmail.com>
2018-11-25 08:19:00 +0000

    Copy the DLL before loading it
    To avoid locking it on Windows.

--- R/on-load.R
+++ R/on-load.R
@@ -2,6 +2,43 @@
 Internal <- NULL
 
 .onLoad <- function(libname, pkgname) {
+
+  libs <- .dynLibs()
+  matchidx <- vapply(libs, "[[", character(1), "name") == pkgname
+  pkglibs <- libs[matchidx]
+  for (lib in pkglibs) {
+    dyn.unload(lib[["path"]])
+  }
+  .dynLibs(libs[!(libs %in% pkglibs)])
+
+  dir.create(tmp <- tempfile(.packageName))
+  tmp <- normalizePath(tmp)
+  dir.create(file.path(tmp, pkgname, "libs"), recursive = TRUE)
+
+  ext <- .Platform$dynlib.ext
+
+  ## This is for pkgload / devtools
+  lib1 <- file.path(libname, pkgname, "src", paste0(pkgname, ext))
+  if (file.exists(lib1)) file.copy(lib1, file.path(tmp, pkgname, "libs"))
+
+  ## This is the proper R CMD INSTALL
+  lib2 <- file.path(libname, pkgname, "libs", paste0(pkgname, ext))
+  if (file.exists(lib2)) file.copy(lib2, file.path(tmp, pkgname, "libs"))
+
+  ## Plus we need these as well
+  file.copy(
+    file.path(libname, pkgname, c("DESCRIPTION", "NAMESPACE")),
+    file.path(tmp, pkgname))
+
+  dll <- library.dynam(pkgname, pkgname, lib.loc = tmp)
+
+  syms <- names(getDLLRegisteredRoutines(dll)$.Call)
+  routines <- getNativeSymbolInfo(syms, dll)
+  ns <- asNamespace(pkgname)
+
+  ns$.__NAMESPACE__.$DLLs[[.packageName]] <- dll
+  for (n in names(routines)) ns[[paste0("c_", n)]] <- routines[[n]]
+
   ## This is to circumvent a ps bug
   if (ps::ps_is_supported()) ps::ps_handle()
   supervisor_reset()
