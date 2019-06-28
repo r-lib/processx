2b9d8d9fde10ce0267431220e252894deaf98da1
Gábor Csárdi <csardi.gabor@gmail.com>
2019-06-28 10:32:13 +0100

    Add mmap test for copy-on-write

--- tests/testthat/test-mmap.R
+++ tests/testthat/test-mmap.R
@@ -56,7 +56,7 @@ test_that("serialize via mmap, subproces", {
   map <- conn_create_mmap(data)
   on.exit(close(map), add = TRUE)
 
-    proc <- callr::r_bg(
+  proc <- callr::r_bg(
     function() {
       bytes <- .Call(asNamespace("processx")$c_processx__mmap_unpack, 3L)
       unserialize(bytes[[1]])
@@ -68,4 +68,45 @@ test_that("serialize via mmap, subproces", {
 
   ret <- proc$get_result()
   expect_identical(mtcars2, ret)
+})
+
+test_that("copy on write", {
+
+  data <- list(
+    1:100, runif(100000), charToRaw("foobar"), 1:100 < sample(1:100))
+  map <- conn_create_mmap(data)
+  on.exit(close(map), add = TRUE)
+
+  proc <- callr::r_bg(
+    function() {
+      data <- .Call(asNamespace("processx")$c_processx__mmap_unpack, 3L)
+      data[[2]][] <- 100
+      data
+    },
+    poll_connection = FALSE,
+    connections = list(map))
+  on.exit(proc$kill(), add = TRUE)
+  proc$wait(1000)
+
+  sub1 <- proc$get_result()
+  expect_true(all(sub1[[2]] == 100))
+
+  # Another subprocess is not affected
+  proc <- callr::r_bg(
+    function() {
+      .Call(asNamespace("processx")$c_processx__mmap_unpack, 3L)
+    },
+    poll_connection = FALSE,
+    connections = list(map))
+  on.exit(proc$kill(), add = TRUE)
+  proc$wait(1000)
+
+  sub2 <- proc$get_result()
+  expect_identical(sub2, data)
+
+  # The shared memory in the parent should not be affected.
+  # We do this at the end, because unpack closes the file descriptor
+  fd <- conn_get_fileno(map)
+  ret <- .Call(c_processx__mmap_unpack, fd)
+  expect_identical(ret, data)
 })
