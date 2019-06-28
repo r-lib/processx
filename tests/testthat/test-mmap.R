7954bc1a4c3a6979de82d6a2909172cff5f7fe9b
Gábor Csárdi <csardi.gabor@gmail.com>
2019-06-28 10:04:17 +0100

    Add two more simple test cases for mmap
    To show how to serialize into shared memory.

--- tests/testthat/test-mmap.R
+++ tests/testthat/test-mmap.R
@@ -34,3 +34,38 @@ test_that("mmap to subprocess", {
   ret <- proc$get_result()
   expect_identical(data, ret)
 })
+
+test_that("serialize via mmap", {
+  skip_other_platforms("unix")
+
+  mtcars2 <- tibble::as_tibble(mtcars, rownames = "name")
+  data <- list(serialize(mtcars2, connection = NULL))
+
+  map <- .Call(c_processx__mmap_pack, tempfile(), data)
+  ret <- unserialize(.Call(c_processx__mmap_unpack, map[[1]])[[1]])
+
+  expect_identical(mtcars2, ret)
+})
+
+test_that("serialize via mmap, subproces", {
+  skip_other_platforms("unix")
+
+  mtcars2 <- tibble::as_tibble(mtcars, rownames = "name")
+  data <- list(serialize(mtcars2, connection = NULL))
+
+  map <- conn_create_mmap(data)
+  on.exit(close(map), add = TRUE)
+
+    proc <- callr::r_bg(
+    function() {
+      bytes <- .Call(asNamespace("processx")$c_processx__mmap_unpack, 3L)
+      unserialize(bytes[[1]])
+    },
+    poll_connection = FALSE,
+    connections = list(map))
+  on.exit(proc$kill(), add = TRUE)
+  proc$wait(1000)
+
+  ret <- proc$get_result()
+  expect_identical(mtcars2, ret)
+})
