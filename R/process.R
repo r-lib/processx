385c0ba1f5fe5a933145cd090d0c9fe8f3a2277c
Gábor Csárdi <csardi.gabor@gmail.com>
2021-03-21 23:00:57 +0100

    More docs about inheriting the standard streams

--- R/process.R
+++ R/process.R
@@ -94,12 +94,16 @@ process <- R6::R6Class(
     #'     provided;
     #'   * a file name, use this file as standard input;
     #'   * `"|"`: create a (writeable) connection for stdin.
-    #'   * `""` (empty string): inherit it from the main R process.
+    #'   * `""` (empty string): inherit it from the main R process. If the
+    #'     main R process does not have a standard input stream, e.g. in
+    #'     RGui on Windows, then an error is thrown.
     #' @param stdout  What to do with the standard output. Possible values:
     #'   * `NULL`: discard it;
     #'   * a string, redirect it to this file;
     #'   * `"|"`: create a connection for it.
-    #'   * `""` (empty string): inherit it from the main R process.
+    #'   * `""` (empty string): inherit it from the main R process. If the
+    #'     main R process does not have a standard output stream, e.g. in
+    #'     RGui on Windows, then an error is thrown.
     #' @param stderr What to do with the standard error. Possible values:
     #'   * `NULL`: discard it;
     #'   * a string, redirect it to this file;
@@ -107,7 +111,9 @@ process <- R6::R6Class(
     #'   * `"2>&1"`: redirect it to the same connection (i.e. pipe or file)
     #'     as `stdout`. `"2>&1"` is a way to keep standard output and error
     #'     correctly interleaved.
-    #'   * `""` (empty string): inherit it from the main R process.
+    #'   * `""` (empty string): inherit it from the main R process. If the
+    #'     main R process does not have a standard error stream, e.g. in
+    #'     RGui on Windows, then an error is thrown.
     #' @param pty Whether to create a pseudo terminal (pty) for the
     #'   background process. This is currently only supported on Unix
     #'   systems, but not supported on Solaris.
