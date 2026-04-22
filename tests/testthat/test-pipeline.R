a3ca564727948128a8fb07a956ad75fb126089b2
Gábor Csárdi <csardi.gabor@gmail.com>
2026-04-22 09:59:27 +0200

    Improve pipeline tests
    - Mark unix specific tests as such.
    - Add some platform independent tests using px.

--- tests/testthat/test-pipeline.R
+++ tests/testthat/test-pipeline.R
@@ -1,5 +1,6 @@
 test_that("2-process pipeline: sort | uniq", {
   skip_on_cran()
+  skip_on_os("windows")
 
   pl <- pipeline$new(
     list(c("sort"), c("uniq")),
@@ -19,6 +20,7 @@ test_that("2-process pipeline: sort | uniq", {
 
 test_that("3-process pipeline: cat | sort | uniq", {
   skip_on_cran()
+  skip_on_os("windows")
 
   pl <- pipeline$new(
     list(c("cat"), c("sort"), c("uniq")),
@@ -38,6 +40,7 @@ test_that("3-process pipeline: cat | sort | uniq", {
 
 test_that("single-process pipeline is equivalent to process$new()", {
   skip_on_cran()
+  skip_on_os("windows")
 
   pl <- pipeline$new(list(c("echo", "hello")), stdout = "|")
   on.exit(pl$kill(), add = TRUE)
@@ -50,6 +53,7 @@ test_that("single-process pipeline is equivalent to process$new()", {
 
 test_that("pipeline is_alive() and get_pids()", {
   skip_on_cran()
+  skip_on_os("windows")
 
   pl <- pipeline$new(
     list(c("sort"), c("cat")),
@@ -70,6 +74,7 @@ test_that("pipeline is_alive() and get_pids()", {
 
 test_that("pipeline get_processes() returns process objects", {
   skip_on_cran()
+  skip_on_os("windows")
 
   pl <- pipeline$new(
     list(c("sort"), c("uniq")),
@@ -88,6 +93,7 @@ test_that("pipeline get_processes() returns process objects", {
 
 test_that("pipeline kill() stops all processes", {
   skip_on_cran()
+  skip_on_os("windows")
 
   pl <- pipeline$new(
     list(c("cat"), c("cat")),
@@ -103,6 +109,7 @@ test_that("pipeline kill() stops all processes", {
 
 test_that("pipeline stdout to file", {
   skip_on_cran()
+  skip_on_os("windows")
 
   tmp <- tempfile()
   on.exit(unlink(tmp), add = TRUE)
@@ -134,4 +141,120 @@ test_that("conn_create_proc_pipepair() returns write/read ends", {
   expect_length(pipe, 2L)
   expect_true(is_connection(pipe[[1]]))
   expect_true(is_connection(pipe[[2]]))
+})
+
+test_that("px single-process pipeline", {
+  skip_on_cran()
+  px <- get_tool("px")
+
+  pl <- pipeline$new(list(c(px, "outln", "hello")), stdout = "|")
+  on.exit(pl$kill(), add = TRUE)
+
+  pl$wait()
+  out <- trimws(pl$read_all_output())
+  expect_equal(out, "hello")
+  expect_equal(pl$get_exit_statuses(), list(0L))
+})
+
+test_that("px 2-process pipeline: passthrough", {
+  skip_on_cran()
+  px <- get_tool("px")
+
+  pl <- pipeline$new(
+    list(c(px, "cat", "<stdin>"), c(px, "cat", "<stdin>")),
+    stdin = "|",
+    stdout = "|"
+  )
+  on.exit(pl$kill(), add = TRUE)
+
+  pl$write_input("hello\nworld")
+  pl$close_input()
+  out <- pl$read_all_output_lines()
+  pl$wait()
+
+  expect_equal(out, c("hello", "world"))
+  expect_equal(pl$get_exit_statuses(), list(0L, 0L))
+})
+
+test_that("px 3-process pipeline: passthrough", {
+  skip_on_cran()
+  px <- get_tool("px")
+
+  pl <- pipeline$new(
+    list(
+      c(px, "cat", "<stdin>"),
+      c(px, "cat", "<stdin>"),
+      c(px, "cat", "<stdin>")
+    ),
+    stdin = "|",
+    stdout = "|"
+  )
+  on.exit(pl$kill(), add = TRUE)
+
+  pl$write_input("hello\nworld")
+  pl$close_input()
+  out <- pl$read_all_output_lines()
+  pl$wait()
+
+  expect_equal(out, c("hello", "world"))
+  expect_equal(pl$get_exit_statuses(), list(0L, 0L, 0L))
+})
+
+test_that("px pipeline is_alive() and get_pids()", {
+  skip_on_cran()
+  px <- get_tool("px")
+
+  pl <- pipeline$new(
+    list(c(px, "cat", "<stdin>"), c(px, "cat", "<stdin>")),
+    stdin = "|",
+    stdout = "|"
+  )
+  on.exit(pl$kill(), add = TRUE)
+
+  expect_true(pl$is_alive())
+  pids <- pl$get_pids()
+  expect_length(pids, 2L)
+  expect_true(all(pids > 0L))
+
+  pl$close_input()
+  pl$wait()
+  expect_false(pl$is_alive())
+})
+
+test_that("px pipeline kill() stops all processes", {
+  skip_on_cran()
+  px <- get_tool("px")
+
+  pl <- pipeline$new(
+    list(c(px, "cat", "<stdin>"), c(px, "cat", "<stdin>")),
+    stdin = "|",
+    stdout = "|"
+  )
+
+  expect_true(pl$is_alive())
+  pl$kill()
+  pl$wait()
+  expect_false(pl$is_alive())
+})
+
+test_that("px pipeline stdout to file", {
+  skip_on_cran()
+  px <- get_tool("px")
+
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+
+  pl <- pipeline$new(
+    list(c(px, "cat", "<stdin>"), c(px, "cat", "<stdin>")),
+    stdin = "|",
+    stdout = tmp
+  )
+  on.exit(pl$kill(), add = TRUE)
+
+  pl$write_input("hello\nworld\n")
+  pl$close_input()
+  pl$wait()
+
+  expect_equal(readLines(tmp), c("hello", "world"))
+  expect_equal(pl$get_exit_statuses(), list(0L, 0L))
 })
