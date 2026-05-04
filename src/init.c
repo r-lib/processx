c5f9afe6448896294ea08a568867d5e580126cab
Michael Chirico <chiricom@google.com>
2026-05-04 11:02:08 -0700

    Cleaner structure of is_asan_ check

--- src/init.c
+++ src/init.c
@@ -43,13 +43,9 @@ SEXP gcov_flush(void) {
 
 SEXP is_asan_()
 {
-#if defined(__has_feature)
-#if __has_feature(address_sanitizer) // for clang
-#define __SANITIZE_ADDRESS__         // GCC already sets this
-#endif
-#endif
-
-#ifdef __SANITIZE_ADDRESS__
+#if defined(__SANITIZE_ADDRESS__)
+  return Rf_ScalarLogical(1);
+#elif defined(__has_feature) && __has_feature(address_sanitizer) // for clang
   return Rf_ScalarLogical(1);
 #else
   return Rf_ScalarLogical(0);
